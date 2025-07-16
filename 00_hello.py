a45bab87b44a4964c70c50560f514f7e7e883969
Alejandro <alejandrocid1218@gmail.com>
2025-07-16 19:31:23 -0400

    Ejemplo 1 corriendo

---
+++ 00_hello.py
@@ -0,0 +1,17 @@
+# Esto es un comentario:
+# Nestro Hola Mundo en python
+print("Hola Python")
+print('Hola Mundo')
+print(type('Hola Mundo'))
+
+"""
+Esto es
+un comentario
+en varias lineas
+"""
+# Consultar el tipo de datos
+print(type('Hola Mundo')) # Tipo 'str'
+print(type(5)) # tipo 'int'
+print(type(1.5)) # tipo 'float'
+print(type(1 + 2j)) # tipo 'complex'
+print(type(True)) # tipo 'bool'
