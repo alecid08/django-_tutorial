0ba4a4f5cba0937018a46870ff2d2642b79e9ff1
Alejandro <alejandrocid1218@gmail.com>
2025-07-16 20:33:05 -0400

    str corriendo

---
+++ 01_variables.py
@@ -0,0 +1,15 @@
+# Variables
+
+my_variable = "My string variable"
+print(my_variable)
+my_int_variable = 5
+print(my_int_variable)
+
+my_bool_variable = True
+print(my_bool_variable)
+
+my_into_to_str_variable = str(my_int_variable)
+print(my_into_to_str_variable)
+print(type(my_into_to_str_variable))
+
+print(my_int_variable,my_int_variable,my_bool_variable)
