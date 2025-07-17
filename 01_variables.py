424dfa2faf8b1f800c89853628fd7cc937125d43
Alejandro <alejandrocid1218@gmail.com>
2025-07-16 20:59:50 -0400

    Concatenando variables ok

--- 01_variables.py
+++ 01_variables.py
@@ -12,4 +12,14 @@ my_into_to_str_variable = str(my_int_variable)
 print(my_into_to_str_variable)
 print(type(my_into_to_str_variable))
 
-print(my_int_variable,my_int_variable,my_bool_variable)
+# Concatenacion de variables en un print
+(print(my_variable,my_int_variable,my_bool_variable))
+
+print("Este es el valor de:",my_bool_variable)
+
+# Algunas Funciones del sistema
+print(len(my_variable))
+
+# Variables en una sola línea
+name, surname, alias, age = "Alejandro", "Cid", "Ale", 17
+print("mi nombre es:", name, ".Mi apellido es:", surname, ".Tengo:", age, "años", ".Mia alias es:", alias)
