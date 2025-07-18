23ad8da5618d5f53c4958bfe98fca3e2612830da
Alejandro <alejandrocid1218@gmail.com>
2025-07-17 22:28:38 -0400

    # Operadores Aritméticos

---
+++ 02_Operadores.py
@@ -0,0 +1,11 @@
+# Operadores Aritméticos 
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 / 4)
+print(10 % 3) 
+print(10 // 3)
+
+print("Hola " + "Python " + "¿Que tal? ")
+print("Hola " + str(5))
+print("Hola " * (2**3))
