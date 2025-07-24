82481e315d03e921b392ca86c341d4cd01fc90c2
Alejandro <alejandrocid1218@gmail.com>
2025-07-23 20:53:28 -0400

    reverse, funciones: strings

--- 03_Strings.py
+++ 03_Strings.py
@@ -26,4 +26,43 @@ print("Mi nombre es {} {} y mi edad es {} ".format(name, surname, age))
 print("Mi nombre es %s %s y mi edad es %d " %(name, surname, age))
 
 print("Mi nombre es {name} {surname} y mi edad es {age}")
+
 print(f"Mi nombre es {name} {surname} y mi edad es {age}")
+
+ # Desempaquetado de caracteres
+
+language = "Python"
+a, b, c, d, e, f = language
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+# División
+language_slice = language[1:3]
+print(language_slice)
+
+language_slice = language[1:]
+print(language_slice)
+
+language_slice = language[-2]
+print(language_slice)
+# Reverse
+
+reversed_language = language[::-1]
+print(reversed_language)
+
+language_slice = language[1:2:4]
+print(language_slice)
+language_slice = language[0:6:2]
+print(language_slice)
+
+# Funciones
+print(language.capitalize())
+print(language.upper())
+print(language.count("t"))
+print(language.isnumeric())
+print("1".isnumeric())
+print(language.lower())
+print(language.upper().isupper())
